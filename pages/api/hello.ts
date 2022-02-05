cc9619647596ae8e5c337f01ce0ff8205450d050
brandonblankenstein <brandon.blankenstein@gmail.com>
2022-02-05 14:56:56 -0600

    added types to hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -16,15 +16,15 @@ const express = require('express')
 const request = require('request')
 const app = express()
 
-app.use((req, res, next) => {
+app.use((req: any, res: { header: (arg0: string, arg1: string) => void }, next: () => void) => {
   res.header('Access-Control-Allow-Origin', '*')
   next()
 })
 
-app.get('/medium', (req, res) => {
+app.get('/medium', (req: any, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { type: string; message: any }): any; new(): any } }; set: (arg0: string, arg1: string) => void; send: (arg0: Buffer) => void }) => {
   request(
     { url: 'https://brandonblankenstein.medium.com/' },
-    (error, response, body) => {
+    (error: any, response: { statusCode: number }, body: WithImplicitCoercion<ArrayBuffer | SharedArrayBuffer>) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({
           type: 'error', message:
